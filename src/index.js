9e7b464ae8a44ce5e023a4493e57542256a28c91
Kecio Caetano Barbosa <kecio.kecio@gmail.com>
2026-03-24 21:02:19 -0400

    setting up basic API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,95 @@
 import 'dotenv/config';
 import express from "express";
 import cors from 'cors';
+import {v4 as uuidv4} from 'uuid'
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.use((req, res, next) => {
+  req.me = users[1];
+  next();
+});
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+app.get('/messages', (req, res)=>{
+  return res.send(Object.values(messages))
+})
+
+app.get('/messages/:messageId', (req, res)=>{
+  return res.send(messages[req.params.messageId])
+})
+
+app.post('/messages', (req, res) =>{
+  const id = uuidv4()
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id
+  }
+
+  messages[id] = message
+  return res.send(message)
+})
+
+app.delete('/messages/:messageId', (req, res) =>{
+  const {
+    [req.params.messageId] : message, 
+    ...otherMessages} = messages
+
+    messages = otherMessages
+    return res.send(messages)
+
+})
+
+app.get("/users", (req, res) => {
+  return res.send(Object.values(users))
+});
+
+app.get("/users/:userId", (req, res) => {
+  return res.send(users[req.params.userId])
+});
+
+app.post('/users/:userId', (req,res) =>{
+  return res.send(`POST HTTP method on user/${req.params.userId} resource`)
+}) 
+
+app.put('/users/:userId', (req, res) =>{
+  return res.send('PUT HTTP method on user resource')
+})
+
+app.delete('/users/:userId', (req, res) =>{
+  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`)
+})
+
+
+
 
 
 app.listen(process.env.PORT, (error) => {
